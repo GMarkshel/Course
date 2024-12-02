35f8575c6f1dd51d0c80d93d3af58e61a398cfd7
GMarkshel <shelpova02@gmail.com>
2024-12-02 03:12:59 +0300

    Create Module_1_6.py

---
+++ Module_1_6.py
@@ -0,0 +1,5 @@
+amound_of_homework = 12
+amound_of_hour = 1.5
+course = 'Python'
+time_of_one_lessons = amound_of_hour / amound_of_homework
+print('Курс:', course, ', Всего задач:', amound_of_homework, ', Затрачено часов:', amound_of_hour, ', Среднее время выполнения:', time_of_one_lessons,'часа' )
